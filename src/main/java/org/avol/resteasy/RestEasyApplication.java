46350b43e9c1d2e402c504156ded684f66aae34d
PLovababu <lovababu.it@gmail.com>
2015-09-06 23:34:04 +0530

    missinge changes

--- src/main/java/org/avol/resteasy/RestEasyApplication.java
+++ src/main/java/org/avol/resteasy/RestEasyApplication.java
@@ -1,13 +1,14 @@
 package org.avol.resteasy;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 /**
  * Created by Lovababu on 9/6/2015.
  */
 
 @ApplicationPath("/rest")
-public class RestEasyApplication {
+public class RestEasyApplication extends Application{
     public RestEasyApplication() {
     }
 }
