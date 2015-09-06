dc57bb015ac46ffd45e8fbeca086e611cbea408e
PLovababu <lovababu.it@gmail.com>
2015-09-06 23:30:35 +0530

    missinge classes

---
+++ src/main/java/org/avol/resteasy/RestEasyApplication.java
@@ -0,0 +1,13 @@
+package org.avol.resteasy;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * Created by Lovababu on 9/6/2015.
+ */
+
+@ApplicationPath("/rest")
+public class RestEasyApplication {
+    public RestEasyApplication() {
+    }
+}
